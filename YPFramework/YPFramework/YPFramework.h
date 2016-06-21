ca23bb6ce51e4f9c358c9ded466a32b4f219c6c1
王云鹏 <wypwelldone@163.com>
2016-06-21 11:07:17 +0800

    2

---
+++ YPFramework/YPFramework/YPFramework.h
@@ -0,0 +1,19 @@
+//
+//  YPFramework.h
+//  YPFramework
+//
+//  Created by maceria on 16/6/21.
+//  Copyright © 2016年 maceria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YPFramework.
+FOUNDATION_EXPORT double YPFrameworkVersionNumber;
+
+//! Project version string for YPFramework.
+FOUNDATION_EXPORT const unsigned char YPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YPFramework/PublicHeader.h>
+
+
---
+++ YPFramework/YPFramework/YPMacroDefines.h
@@ -0,0 +1,16 @@
+//
+//  YPMacroDefines.h
+//  YPFramework
+//
+//  Created by maceria on 16/6/21.
+//  Copyright © 2016年 maceria. All rights reserved.
+//
+
+#ifndef YPMacroDefines_h
+#define YPMacroDefines_h
+
+//获取屏幕 宽度、高度
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
+#endif /* YPMacroDefines_h */
